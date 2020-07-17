77572190b62bb5c1222899afa8abf21ce7e151bf
aybuthel <67958488+aybuthel@users.noreply.github.com>
2020-07-17 11:10:49 +0200

    Update ft_ft.c

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -11,9 +11,8 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void ft_ft(int *nbr)
+void	ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
